75f058e4a0e8bef5acd7485bb6472e7a3b00a51b
OlegKrivenko <krivenko.olegus@gmail.com>
2023-03-07 16:41:53 +0200

    task 1 has done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const ref = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+ref.startBtn.addEventListener('click', () => {
+  changeBodyColor();
+
+  timerId = setInterval(() => {
+    changeBodyColor();
+  }, 1000);
+
+  ref.startBtn.setAttribute('disabled', '');
+  ref.stopBtn.removeAttribute('disabled');
+});
+
+ref.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  ref.startBtn.removeAttribute('disabled');
+  ref.stopBtn.setAttribute('disabled', '');
+});
+
+function changeBodyColor() {
+  ref.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
